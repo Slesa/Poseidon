00f5dade4cdd7d1fca4252491496efb61b4f8330
J. Preiss <joerg.preiss@me.com>
2012-10-11 22:31:46 +0200

    Added user model, translations, some events

---
+++ src/Backoffice.Prism/Ums.OfficeModule/Events/UserRoleEvents.cs
@@ -0,0 +1,24 @@
+﻿using Ums.Model;
+
+namespace Poseidon.Ums.OfficeModule.Events
+{
+    public class UserRoleChangedEvent
+    {
+        public UserRoleChangedEvent(UserRole userRole)
+        {
+            UserRole = userRole;
+        }
+
+        protected UserRole UserRole { get; private set; }
+    }
+
+    public class UserRoleRemovedEvent
+    {
+        public UserRoleRemovedEvent(int id)
+        {
+            Id = id;
+        }
+
+        protected int Id { get; private set; }
+    }
+}
