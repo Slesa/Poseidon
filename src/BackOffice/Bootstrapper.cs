7b3bec9548273415ae09f0dfc05c02647bb3e314
J. Preiss <joerg.preiss@me.com>
2013-06-06 22:13:05 +0200

    #1 Added bootstrapper with log4net configuration to frontend #3 Added bootstrapper with log4net configuration to backend

---
+++ src/BackOffice/Bootstrapper.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using System.Windows;
+using Microsoft.Practices.Prism.Logging;
+using Microsoft.Practices.Prism.Regions;
+using Microsoft.Practices.Prism.UnityExtensions;
+using Microsoft.Practices.ServiceLocation;
+using Poseidon.BackOffice.Views;
+using Poseidon.Common;
+
+namespace Poseidon.BackOffice
+{
+    public class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return ServiceLocator.Current.GetInstance<Shell>();
+        }
+
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+
+            var logFile = new FileInfo("Poseidon.BackOffice.log4net.config");
+            log4net.Config.XmlConfigurator.Configure(logFile);
+
+            Application.Current.MainWindow = (Window)Shell;
+            Application.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureContainer()
+        {
+            base.ConfigureContainer();
+            RegisterShellObjects();
+        }
+
+        void RegisterShellObjects()
+        {
+            RegisterTypeIfMissing(typeof(ILoggerFacade), typeof(Log4NetLogger), true);
+            RegisterTypeIfMissing(typeof(IRegionManager), typeof(IRegionManager), true);
+        }
+    }
+}
