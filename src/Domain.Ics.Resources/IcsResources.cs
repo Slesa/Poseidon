cc4806e5ac8d8671816b96b382fb26cb8c633db1
J. Preiss <joerg.preiss@slesa.de>
2013-07-15 07:01:52 +0200

    Added ICS resources

---
+++ src/Domain.Ics.Resources/IcsResources.cs
@@ -0,0 +1,15 @@
+﻿namespace Poseidon.Domain.Ums.Resources
+{
+    public static class UmsResources
+    {
+        public static readonly string IcsIcon = @"/Poseidon.Domain.Ics.Resources;component/Ics.png";
+
+        public static readonly string ProductionItemIcon = @"/Poseidon.Domain.Ics.Resources;component/ProductionItem.png";
+        public static readonly string PurchaseFamilyIcon = @"/Poseidon.Domain.Ics.Resources;component/PurchaseFamily.png";
+        public static readonly string PurchaseItemIcon = @"/Poseidon.Domain.Ics.Resources;component/PurchaseItem.png";
+        public static readonly string RecipeItemIcon = @"/Poseidon.Domain.Ics.Resources;component/RecipeItem.png";
+        public static readonly string StockItemIcon = @"/Poseidon.Domain.Ics.Resources;component/StockItem.png";
+        public static readonly string UnitIcon = @"/Poseidon.Domain.Ics.Resources;component/Unit.png";
+        public static readonly string UnitTypeIcon = @"/Poseidon.Domain.Ics.Resources;component/UnitType.png";
+    }
+}
---
+++ src/Domain.Ics.Resources/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Domain.Ics.Resources")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("06435499-0575-4dd2-ba24-2ad4aeeec3a7")]
