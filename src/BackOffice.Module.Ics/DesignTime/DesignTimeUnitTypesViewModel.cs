db75a4cd08c8184f0724865c5d2c002ed088e101
J. Preiss <joerg.preiss@slesa.de>
2013-07-18 08:21:38 +0200

    Trying to use XML data source in design mode

---
+++ src/BackOffice.Module.Ics/DesignTime/DesignTimeUnitTypesViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Data;
+using Poseidon.Domain.Ics.Model;
+
+namespace Poseidon.BackOffice.Module.Ics.DesignTime
+{
+    public class DesignTimeUnitTypesViewModel
+    {
+        public IEnumerable<UnitType> UnitTypes
+        {
+            get
+            {
+                var ds = new XmlDataProvider();
+                ds.Source = new Uri("/DesignTime/DesignTimeUnitTypes.xml");
+                var result = ds.Data;
+                return result as IEnumerable<UnitType>;
+            }
+        }
+    }
+}
