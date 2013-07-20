7caefcf44ba85c779c9b5b502ac9cb081876b432
J. Preiss <joerg.preiss@me.com>
2013-07-21 00:22:04 +0200

    Adding first edit view

--- src/BackOffice.Module.Ics/ViewModels/UnitTypesViewModel.cs
+++ src/BackOffice.Module.Ics/ViewModels/UnitTypesViewModel.cs
@@ -1,10 +1,47 @@
 ﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Microsoft.Practices.Prism.Commands;
 using Poseidon.Domain.Ics.Model;
 
 namespace Poseidon.BackOffice.Module.Ics.ViewModels
 {
     public class UnitTypesViewModel
     {
+        public UnitTypesViewModel()
+        {
+            NewUnitTypeCommand = new DelegateCommand(OnNewUnitType);
+            EditUnitTypeCommand = new DelegateCommand(OnEditUnitType, CanEditUnitType);
+            DelUnitTypeCommand = new DelegateCommand(OnDelUnitType, CanDelUnitType);
+        }
+
         public ObservableCollection<UnitType> UnitTypes { get; set; }
+
+        public ICommand NewUnitTypeCommand { get; set; }
+
+        void OnNewUnitType()
+        {
+        }
+
+        public ICommand EditUnitTypeCommand { get; set; }
+
+        void OnEditUnitType()
+        {
+        }
+
+        bool CanEditUnitType()
+        {
+            return true;
+        }
+
+        public ICommand DelUnitTypeCommand { get; set; }
+
+        void OnDelUnitType()
+        {
+        }
+
+        bool CanDelUnitType()
+        {
+            return true;
+        }
     }
 }
