e067a938740e405f33ba6a0164dd39b1933d77e7
J. Preiss <joerg.preiss@me.com>
2014-02-19 22:15:25 +0100

    Remove old prism backoffice try

--- src/Backoffice.Prism/Backoffice/ViewModels/ShellViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Poseidon.Backoffice.ViewModels
-{
-    public class ShellViewModel
-    {
-        public string AppTitle { get { return "Poseidon Backoffice"; } }
-        public string AppIcon { get { return "/Poseidon.Backoffice;component/Resources/Office.png"; } } 
-    }
-}
