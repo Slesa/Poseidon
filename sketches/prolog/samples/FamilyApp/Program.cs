435ef38b59c64ee2ac3a090b230456852553b365
J. Preiss <joerg.preiss@me.com>
2014-03-19 22:32:05 +0100

    Added Prolog.NET samples

---
+++ sketches/prolog/samples/FamilyApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using Axiom.Runtime;
+using Prolog.Assembly;
+
+namespace FamilyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Family familyDB = new Family();
+            
+            if(familyDB.son("jack", "tom"))
+                System.Console.WriteLine("Yeah!");
+
+            System.Console.WriteLine("Who is the father of john? ");
+
+            AbstractTerm f = new AbstractTerm();
+            familyDB.father(f, "john");
+
+            System.Console.WriteLine(f);
+        }
+    }
+}
