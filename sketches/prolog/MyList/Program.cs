77fca3a1853487b07986eb70d305c55d95ac5b60
J. Preiss <joerg.preiss@slesa.de>
2014-03-20 08:24:27 +0100

    Use examples of the other prolog.net implementation.

---
+++ sketches/prolog/MyList/Program.cs
@@ -0,0 +1,23 @@
+﻿using Axiom.Runtime;
+using Prolog.Assembly;
+
+namespace MyList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new PrologList();
+
+            var a = new AbstractTerm();
+            var b = new AbstractTerm();
+            var c = new AbstractTerm();
+            list.runme(a, b, c);
+
+            System.Console.WriteLine(a);
+            System.Console.WriteLine(b);
+            System.Console.WriteLine(c);
+            System.Console.ReadKey();
+        }
+    }
+}
