819e0ab4d6929fefffb8c551935ad8508b335533
Jörg Preiß <joerg.preiss@centigrade.de>
2014-08-29 16:23:30 +0200

    Use SharedResourceDictionary in XAML, but leave implementation empty

---
+++ src/BackOffice/BackOffice.Common/Helpers/DesignResourceDictionary.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Poseidon.BackOffice.Common.Helpers
+{
+    public class DesignResourceDictionary
+#if DEBUG
+        : ResourceDictionary
+#endif
+    {
+         
+    }
+}
