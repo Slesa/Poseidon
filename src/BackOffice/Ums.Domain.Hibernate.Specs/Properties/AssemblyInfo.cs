ab26ee8803756c86ee282b7fafeee86ece3bd763
J. Preiss <joerg.preiss@me.com>
2014-09-06 00:12:03 +0200

    Adjusted versionInfo

--- src/BackOffice/Ums.Domain.Hibernate.Specs/Properties/AssemblyInfo.cs
+++ src/BackOffice/Ums.Domain.Hibernate.Specs/Properties/AssemblyInfo.cs
@@ -8,29 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Ums.Domain.Hibernate.Specs")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Ums.Domain.Hibernate.Specs")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("bfbe49ca-4526-4215-ad2e-1ac382655c32")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
--- src/BackOffice/Ums.Domain.Hibernate/Properties/AssemblyInfo.cs
+++ src/BackOffice/Ums.Domain.Hibernate/Properties/AssemblyInfo.cs
@@ -8,29 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Ums.Domain.Hibernate")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Ums.Domain.Hibernate")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("663fac9c-e587-4b2e-a9f6-dcba9bb3e6ca")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
