69102e4ff3dd8f5d29a466412753fb589b61c9eb
J. Preiss <joerg.preiss@slesa.de>
2014-09-30 21:06:42 +0200

    Issue #pos-25: reactivate tests, quit batch on active AppVeyor, added yml file for AppVeyor

--- build.cmd
+++ build.cmd
@@ -11,6 +11,7 @@ IF NOT [%1]==[] (set TARGET="%1")
 
 rem Bail if we're running a TeamCity build.
 if defined TEAMCITY_PROJECT_NAME goto Quit
+if defined APPVEYOR goto Quit
 
 rem Loop the build script.
 set CHOICE=nothing
