6d9b0c03b57aede94693578c12e64d7ff93de167
J. Preiss <joerg.preiss@slesa.de>
2014-10-12 21:46:10 +0200

    Issue #23: trying to use controls with System.Security

---
+++ sketches/wpf/Login/WpfAuthentication/WpfAuthentication/Views/ViewControl.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Permissions;
+using System.Windows;
+using System.Windows.Controls;
+using WpfAuthentication.Contracts;
+
+namespace WpfAuthentication.Views
+{
+    [PrincipalPermission(SecurityAction.Demand)]
+    public partial class ViewControl : UserControl, IView
+    {
+        public ViewControl()
+        {
+            InitializeComponent();
+        }
+
+        public IViewModel ViewModel
+        {
+            get { return DataContext as IViewModel; }
+            set { DataContext = value; }
+        }
+
+        public void Show()
+        {
+            Visibility = Visibility.Visible;
+        }
+    }
+}
