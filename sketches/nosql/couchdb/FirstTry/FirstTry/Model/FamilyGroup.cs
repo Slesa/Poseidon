48693bd4f9c4a342e12d40b3d50c1263193b3c47
Jörg Preiß <joerg.preiss@slesa.de>
2015-01-16 16:00:40 +0100

    Trying out CouchDB

---
+++ sketches/nosql/couchdb/FirstTry/FirstTry/Model/FamilyGroup.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace FirstTry.Model
+{
+    public class FamilyGroup
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public static List<FamilyGroup> CreateFamilyGroups()
+        {
+            var result = new List<FamilyGroup>();
+            for (int i = 1; i < 10; i++)
+            {
+                var familyGroup = new FamilyGroup { Id = i, Name = "Family " + i };
+                result.Add(familyGroup);
+            }
+            return result;
+        }
+    }
+}
